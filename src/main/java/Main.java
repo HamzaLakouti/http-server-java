728b25a64d4ae6aa8a9b2a88233c5ae85a43b50b
Hamza Lakouti <hamza.lakouti@gmail.com>
2024-03-31 03:02:51 +0000

    Extract HTTP request path

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,8 @@
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
+
+import java.util.Arrays;
 
 public class Main {
   public static void main(String[] args) {
@@ -15,14 +15,37 @@ public class Main {
     
     try {
       serverSocket = new ServerSocket(4221);
-      serverSocket.setReuseAddress(true);
-      clientSocket = serverSocket.accept(); 
-
-      OutputStream output = clientSocket.getOutputStream();
-      String textResponse = "HTTP/1.1 200 OK\r\n\r\n";
-      output.write(textResponse.getBytes(StandardCharsets.UTF_8));
+      while (true) {
+        serverSocket.setReuseAddress(true);
+        clientSocket = serverSocket.accept();
+        System.out.println("accepted new connection");
+        handleConnection(clientSocket);
+      }
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     }
+  }
+
+  private static void handleConnection(final Socket clientSocket) {
+    String foundResponse = "HTTP/1.1 200 OK\r\n\r\n";
+    String notFoundResponse = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+    try (BufferedReader reader = new BufferedReader(
+            new InputStreamReader(clientSocket.getInputStream()));
+         OutputStream outputStream = clientSocket.getOutputStream()) {
+      String path = reader.readLine().split(" ")[1];
+      System.out.println("Requested path: " + path);
+      if (path.equals("/")) {
+        outputStream.write(foundResponse.getBytes());
+      } else {
+        outputStream.write(notFoundResponse.getBytes());
+      }
+      outputStream.flush();
+    } catch (IOException e) {
+      System.err.println("exception occurred. " + e.getMessage());
+    }
+  }
+  private static byte[] encodeOutput(String value) {
+    final String CRLF = "\r\n";
+    return String.format("%s%s%s", value, CRLF, CRLF).getBytes();
   }
 }
