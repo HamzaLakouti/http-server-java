548c6104c04ed1959e9581df511d1b405ee859f6
Hamza Lakouti <hamza.lakouti@gmail.com>
2024-04-01 23:44:28 +0000

    Add respond with body

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-import java.util.Arrays;
 
 public class Main {
   public static void main(String[] args) {
@@ -35,7 +34,15 @@ public class Main {
       String path = reader.readLine().split(" ")[1];
       System.out.println("Requested path: " + path);
       if (path.equals("/")) {
-        outputStream.write(foundResponse.getBytes());
+        outputStream.write(foundResponse.getBytes()); }
+      else if (path.contains("/echo/")) {
+          final String response = path.split("/echo/")[1];
+          outputStream.write("HTTP/1.1 200 OK\r\n".getBytes());
+          outputStream.write("Content-Type: text/plain\r\n".getBytes());
+          outputStream.write(
+                  ("Content-Length: " + response.length() + "\r\n").getBytes());
+          outputStream.write("\r\n".getBytes());
+          outputStream.write(response.getBytes());
       } else {
         outputStream.write(notFoundResponse.getBytes());
       }
